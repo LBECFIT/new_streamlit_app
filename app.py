a19e156160625de8b0d026aa91880a5737b80a8c
LBECFIT <87069036+LBECFIT@users.noreply.github.com>
2022-06-30 16:04:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,13 @@ import plotly.figure_factory as ff
 from plotly.subplots import make_subplots
 import plotly.express as px
 
+from waste_graph_functions import *
+from streamlit_option_menu import option_menu
+
+from list_weekly_kpis import *
+
+from impact_report_metrics import *
+
 df = pd.read_csv('df_covers.csv')
 
 st.title('New test app')
