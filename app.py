549c2eaa6987b5dda6ac4a69c8445528894a1c99
LBECFIT <87069036+LBECFIT@users.noreply.github.com>
2022-06-30 17:14:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -655,6 +655,458 @@ if selected == 'Missing data':
 		fig_individual_heatmap = ff.create_annotated_heatmap(z,x=x,y=y, colorscale=colorscale,annotation_text=z_text)
 
 		st.plotly_chart(fig_individual_heatmap,use_container_width=True)
+		
+##############################################################
+##############################################################
+##############################################################
+
+if selected == 'Impact Report':
+	df_kitchen = pd.read_csv('df_kitchen.csv',sep=',')
+	df_companies = pd.read_csv('df_companies.csv',sep=',')
+	df_rf3 = pd.read_csv('df_rf3.csv',sep=',')
+	df_covers = pd.read_csv('df_covers.csv',sep = ',')
+	fit_raw_data = pd.read_csv('fit_raw_data.csv',sep=',')
+
+	liste_company = list(df_rf3['id_company'].unique())
+
+
+	col_impact_1, col_impact_2 = st.columns(2)
+
+	with col_impact_1:
+
+		focus_company =st.selectbox('Choose a company',(liste_company))
+
+
+	df_filtered_company = df_rf3[df_rf3['id_company']==focus_company]
+	liste_kitchen = list(df_filtered_company['kitchen'].unique())
+
+
+	with col_impact_2:
+
+		focus_kitchen = st.selectbox('Choose a kitchen',(liste_kitchen))
+
+	col_dates_1, col_dates_2 = st.columns(2)
+
+
+	with col_dates_2:
+		end_date = st.date_input("Select end date for data collection",date(2022, 6, 6))
+
+	with col_dates_1:
+		starting_date = st.date_input("Select start date for data collection",date(2022, 6, 6))
+
+
+
+
+
+	################################### GETTING THE BASELINE START AND END DATES
+
+	df_baseline = fit_raw_data[fit_raw_data["id_company"].str.startswith(focus_company, na = False)].reset_index()
+	df_baseline = fit_raw_data[fit_raw_data["kitchen"].str.startswith(focus_kitchen, na = False)].reset_index()
+
+	df_baseline['baseline_start'] = pd.to_datetime(df_baseline['baseline_start'], format='%Y-%m-%d').dt.floor('d')
+	df_baseline['baseline_end'] = pd.to_datetime(df_baseline['baseline_end'], format='%Y-%m-%d').dt.floor('d')
+
+	baseline_start = df_baseline.loc[0]['baseline_start']
+	baseline_end = df_baseline.loc[0]['baseline_end']
+
+	list_baseline = [(baseline_end - timedelta(days=x)).strftime("%Y-%m-%d") for x in range(((baseline_end-baseline_start).days)+1)]
+	list_baseline.reverse()
+
+	dates_to_remove = st.multiselect(
+     'What day(s) do you want to take out of the baseline ?',
+     list_baseline,
+     [])
+
+	list_baseline = [x for x in list_baseline if x not in dates_to_remove]
+
+	################################### FILTERING FIT RAW DATA
+
+
+	df_companies_hbrand = df_companies[df_companies["id_company"].str.startswith(focus_company, na = False)].reset_index(drop=True)
+	df_kitchen_hbrand = df_kitchen[df_kitchen["id_company"].str.startswith(focus_company, na = False)].reset_index(drop=True)
+
+	df_kitchen_hbrand = df_kitchen_hbrand[df_kitchen_hbrand["kitchen"].str.startswith(focus_kitchen, na = False)].reset_index(drop=True)
+
+	user_data_hbrand = pd.merge(df_companies_hbrand,df_kitchen_hbrand,how = 'right',
+	                    left_on=['id_company'],
+	                    right_on=['id_company'],
+	                    suffixes=("__companies", "__kitchen"),
+	                    copy=True)
+
+	fit_raw_data =user_data_hbrand.copy()
+
+	fit_raw_data['id_company+kitchen']=fit_raw_data['id_company']+'__'+fit_raw_data['kitchen']
+	user_data_hbrand['id_company+kitchen']=user_data_hbrand['id_company']+'__'+user_data_hbrand['kitchen']
+
+	################################### FILTERING RF3
+
+	df_rf3 = df_rf3[df_rf3["id_company"].str.startswith(focus_company, na = False)]
+	df_rf3 = df_rf3[df_rf3["kitchen"].str.startswith(focus_kitchen, na = False)]
+
+	df_rf3['date_waste'] = pd.to_datetime(df_rf3['date_waste']).dt.floor('d')
+	df_rf3['id_company+kitchen']=df_rf3['id_company']+'__'+df_rf3['kitchen']
+	df_rf3 = df_rf3[(df_rf3['date_waste']>=starting_date.strftime('%Y-%m-%d'))&(df_rf3['date_waste']<=end_date.strftime('%Y-%m-%d'))]
+
+
+	################################### FILTERING COVERS
+
+	df_covers = df_covers[df_covers["id_company"].str.startswith(focus_company, na = False)]
+	df_covers = df_covers[df_covers["kitchen"].str.startswith(focus_kitchen, na = False)]
+
+	df_covers['covers'] = df_covers['covers'].astype(int)
+	df_covers['date_waste'] = pd.to_datetime(df_covers['date_waste']).dt.floor('d')
+	df_covers['id_company+kitchen']=df_covers['id_company']+'__'+df_covers['kitchen']
+	df_covers = df_covers[(df_covers['date_waste']>=starting_date.strftime('%Y-%m-%d'))&(df_covers['date_waste']<=end_date.strftime('%Y-%m-%d'))]
+
+
+	################################### MERGING COVERS AND FOOD WASTE
+
+	df_rf3['weight'] = df_rf3['weight'].astype(float)
+	df_rf3['date_waste'] = pd.to_datetime(df_rf3['date_waste'], format='%Y-%m-%d').dt.floor('d')
+	df_covers['date_waste'] = pd.to_datetime(df_covers['date_waste'], format='%Y-%m-%d').dt.floor('d')
+	df_covers = df_covers.groupby(['id_company','shift','date_waste','kitchen'])['covers'].sum().reset_index()
+
+	df = pd.merge(df_rf3, df_covers,how = 'left',
+	    left_on=['id_company', 'date_waste', 'kitchen', 'shift'],
+	    right_on=['id_company', 'date_waste', 'kitchen', 'shift'],
+	    suffixes=("__rf3", "__covers"),
+	    copy=True
+	)
+
+	################################### DISPLAY FW RECORDED + TOTAL COVERS + TOTAL INPUTS
+
+
+	df_tar = df[(df['date_waste']>=baseline_start.strftime("%Y-%m-%d"))&(df['date_waste']<=end_date.strftime("%Y-%m-%d"))]
+	df_tar = df_tar.groupby(['id_company','kitchen','date_waste','shift','covers'])['weight'].sum().reset_index()
+	col_metrics_fw_rec, col_tot_covers, col_nb_inputs = st.columns(3)
+	col_metrics_fw_rec.metric("Food waste recorded (Kg)", round(df_tar.weight.sum()))
+	col_tot_covers.metric("Total covers served", round(df_tar.covers.sum()))
+	col_nb_inputs.metric("Number of food waste entries", df.shape[0])
+
+	################################### DISPLAY BASELINE, AFTER BASELINE AND VARIATION
+
+	company = focus_company+'__'+focus_kitchen
+
+	df_baseline['id_company+kitchen'] = company
+
+	waste_byday = df.copy()
+	base = df_baseline.copy()
+
+	base = base[(base['id_company+kitchen'] == company)].reset_index()
+
+	#list_baseline = ['2022-01-27','2022-01-28','2022-02-03','2022-02-04','2022-02-05','2022-02-06','2022-02-07','2022-02-08','2022-02-09']
+	waste_byday['date_waste'] = pd.to_datetime(waste_byday['date_waste'], format='%Y-%m-%d').dt.floor('d')
+	waste_byday = waste_byday[waste_byday['date_waste'].isin(list_baseline)]
+
+	waste_byday = waste_byday[(waste_byday['id_company+kitchen'] == company)]
+	waste_byday['weight'] = waste_byday['weight'].astype(float)
+	waste_byday = waste_byday.dropna()
+	waste_byday = waste_byday.groupby(['id_company+kitchen','date_waste','covers','shift'])['weight'].sum().reset_index()
+	waste_byday = waste_byday.groupby(['date_waste','shift'])['weight','covers'].sum().reset_index()
+	waste_byday_bf = waste_byday[(waste_byday['shift'] == 'Breakfast')]
+	waste_byday_lunch = waste_byday[(waste_byday['shift'] == 'Lunch')]
+	waste_byday_din = waste_byday[(waste_byday['shift'] == 'Dinner')]
+
+	covers_sum = waste_byday['covers'].sum()
+	waste_sum = waste_byday['weight'].sum()
+	print(covers_sum,waste_sum)
+	#waste_sum_baseline_count +=waste_sum
+	#covers_sum_baseline_count +=covers_sum
+	if covers_sum == 0 or waste_sum ==0 : 
+	    per_cover_avg = 0 
+	else : 
+	    per_cover_avg = round((waste_sum*1000)/covers_sum)
+
+
+
+	covers_sum_bf = waste_byday_bf['covers'].sum()
+	waste_sum_bf = waste_byday_bf['weight'].sum()
+	#print(covers_sum_bf,waste_sum_bf)
+
+	if waste_sum_bf == 0 or covers_sum_bf ==0 : 
+	    per_cover_avg_bf = 0 
+	else : 
+	    per_cover_avg_bf = round((waste_sum_bf*1000)/covers_sum_bf)
+
+
+	covers_sum_lunch = waste_byday_lunch['covers'].sum()
+	waste_sum_lunch = waste_byday_lunch['weight'].sum()
+	#print(covers_sum_lunch,waste_sum_lunch)
+
+	if waste_sum_lunch == 0 or covers_sum_lunch ==0 : 
+	    per_cover_avg_lunch = 0 
+	else : 
+	    per_cover_avg_lunch = round((waste_sum_lunch*1000)/covers_sum_lunch)
+
+	covers_sum_din = waste_byday_din['covers'].sum()
+	waste_sum_din = waste_byday_din['weight'].sum()
+
+	if waste_sum_din == 0 or covers_sum_din ==0 : 
+	    per_cover_avg_din = 0 
+	else : 
+	    per_cover_avg_din = round((waste_sum_din*1000)/covers_sum_din)
+
+	print(waste_sum,covers_sum,per_cover_avg,per_cover_avg_bf,per_cover_avg_lunch,per_cover_avg_din,'breakfast '+'{}'.format(company)+ '= ' + '{}'.format(per_cover_avg_bf),
+	'lunch '+'{}'.format(company)+ '= ' + '{}'.format(per_cover_avg_lunch),
+	'dinner '+'{}'.format(company)+ '= ' + '{}'.format(per_cover_avg_din),
+	'overall '+'{}'.format(company)+ '= ' + '{}'.format(per_cover_avg))
+
+
+	def waste_test_p(company) : 
+	    waste_byday = df.copy()
+	    base = df_baseline.copy()
+	    base = base[(base['id_company+kitchen'] == company)].reset_index()
+
+	    baseline_end = base.loc[0]['baseline_end']
+
+	    baseline_end = baseline_end.strftime("%Y-%m-%d")
+
+	    baseline_end = datetime.strptime(baseline_end,'%Y-%m-%d')
+
+	    waste_byday['date_waste'] = pd.to_datetime(waste_byday['date_waste'], format='%Y-%m-%d').dt.floor('d')
+	    waste_byday = waste_byday[(waste_byday['date_waste'] > baseline_end )&(waste_byday['id_company+kitchen'] == company)&(waste_byday['date_waste'] <= end_date.strftime('%Y-%m-%d'))]
+	    waste_byday['weight'] = waste_byday['weight'].astype(float)
+	    waste_byday = waste_byday.dropna()
+	    waste_byday = waste_byday.groupby(['id_company+kitchen','date_waste','covers','shift','kitchen'])['weight'].sum().reset_index()
+	    waste_byday = waste_byday.groupby(['date_waste'])['weight','covers'].sum().reset_index()
+
+	    covers_sum = waste_byday['covers'].sum()
+	    waste_sum = waste_byday['weight'].sum()
+	    print(company,"covers_sum: {}".format(covers_sum),"waste_sum: {}".format(waste_sum))
+
+	    per_cover_avg = (waste_sum*1000)/covers_sum
+	    per_cover_avg = round(per_cover_avg)
+	    
+	    return(per_cover_avg,covers_sum,waste_sum)
+    
+	per_cover_after_baseline = waste_test_p(company)[0]
+
+	variation = round(((per_cover_after_baseline - per_cover_avg)/per_cover_avg)*100)
+
+
+	col_per_cover_avg, col_per_cover_after_baseline = st.columns(2)
+	col_per_cover_avg.metric("Baseline", str(per_cover_avg)+" g/cover")
+	col_per_cover_after_baseline.metric("After baseline",str(per_cover_after_baseline)+" g/cover",str(variation)+"%",delta_color="inverse")
+
+	################################### DF NEW
+
+	baseline_df = pd.DataFrame(
+	{"id_company+kitchen" : [company],
+	"fw/cover baseline" : [per_cover_avg],
+	"fw/cover baseline bf" : [per_cover_avg_bf],
+	"fw/cover baseline lunch" : [per_cover_avg_lunch],
+	"fw/cover baseline dinner" : [per_cover_avg_din]})
+
+	df_new = pd.merge(df, baseline_df,how = 'left',
+	    left_on=['id_company+kitchen'],
+	    right_on=['id_company+kitchen'],
+	    suffixes=("__df", "__baseline"),
+	    copy=True
+	)
+
+	################################### PREPARE TABLE SAINGS PER MONTH
+
+
+	kg_saved = df_new.copy()
+	base = df_baseline.copy()
+
+	base = base[(base['id_company+kitchen'] == company)].reset_index()
+	baseline_end = base.loc[0]['baseline_end']
+	coef = base.loc[0]['cost_kilo']
+
+	baseline_end = baseline_end.strftime("%Y-%m-%d")
+
+	baseline_end = datetime.strptime(baseline_end,'%Y-%m-%d')
+	print(baseline_end)
+
+
+	kg_saved = kg_saved[(kg_saved['date_waste'] > baseline_end)]
+	################# ENTER BELOW THE END DATE 
+	kg_saved = kg_saved[(kg_saved['date_waste'] <= end_date.strftime("%Y-%m-%d"))]
+
+
+	kg_saved['date_waste'] = pd.to_datetime(kg_saved['date_waste']).dt.floor('d')
+
+	kg_saved['weight'] = kg_saved['weight'].astype(float)
+
+	kg_saved = kg_saved[kg_saved['id_company+kitchen'] == company]
+
+	#Breakfast
+	kg_saved_bf = kg_saved[(kg_saved['shift']=='Breakfast')]
+	kg_saved_bf = kg_saved_bf.groupby(['id_company+kitchen','date_waste','covers','fw/cover baseline bf'])['weight'].sum().reset_index()
+	kg_saved_bf = kg_saved_bf.groupby(['id_company+kitchen','date_waste','fw/cover baseline bf'])['weight','covers'].sum().reset_index()
+	kg_saved_bf['fw/cover'] = (kg_saved_bf['weight']*1000)/kg_saved_bf['covers']
+	kg_saved_bf['kg_saved'] = ((kg_saved_bf['fw/cover baseline bf'] - kg_saved_bf['fw/cover'])*kg_saved_bf['covers'])/1000
+
+	#Lunch
+	kg_saved_lunch = kg_saved[(kg_saved['shift']=='Lunch')]
+	kg_saved_lunch = kg_saved_lunch.groupby(['id_company+kitchen','date_waste','covers','fw/cover baseline lunch'])['weight'].sum().reset_index()
+	kg_saved_lunch = kg_saved_lunch.groupby(['id_company+kitchen','date_waste','fw/cover baseline lunch'])['weight','covers'].sum().reset_index()
+	kg_saved_lunch['fw/cover'] = (kg_saved_lunch['weight']*1000)/kg_saved_lunch['covers']
+	kg_saved_lunch['kg_saved'] = ((kg_saved_lunch['fw/cover baseline lunch'] - kg_saved_lunch['fw/cover'])*kg_saved_lunch['covers'])/1000
+
+
+	#Dinner
+	kg_saved_din = kg_saved[(kg_saved['shift']=='Dinner')]
+	kg_saved_din = kg_saved_din.groupby(['id_company+kitchen','date_waste','covers','fw/cover baseline dinner'])['weight'].sum().reset_index()
+	kg_saved_din = kg_saved_din.groupby(['id_company+kitchen','date_waste','fw/cover baseline dinner'])['weight','covers'].sum().reset_index()
+	kg_saved_din['fw/cover'] = (kg_saved_din['weight']*1000)/kg_saved_din['covers']
+	kg_saved_din['kg_saved'] = ((kg_saved_din['fw/cover baseline dinner'] - kg_saved_din['fw/cover'])*kg_saved_din['covers'])/1000
+
+	kg_saved_bf['month'] = pd.DatetimeIndex(kg_saved_bf['date_waste']).month
+	kg_saved_lunch['month'] = pd.DatetimeIndex(kg_saved_lunch['date_waste']).month
+	kg_saved_din['month'] = pd.DatetimeIndex(kg_saved_din['date_waste']).month
+
+	kg_saved_bf["month"].replace({ 1 : "January",
+	                            2: "February",
+	                            3: "March",
+	                            4: "April",
+	                            5: "May",
+	                            6: "June",
+	                            7: "July",
+	                            8: "August",
+	                            9: "September",
+	                            10: "October",
+	                            11: "November",
+	                            12 : "December"}, inplace=True)
+	kg_saved_lunch["month"].replace({ 1 : "January",
+	                            2: "February",
+	                            3: "March",
+	                            4: "April",
+	                            5: "May",
+	                            6: "June",
+	                            7: "July",
+	                            8: "August",
+	                            9: "September",
+	                            10: "October",
+	                            11: "November",
+	                            12 : "December"}, inplace=True)
+	kg_saved_din["month"].replace({ 1 : "January",
+	                            2: "February",
+	                            3: "March",
+	                            4: "April",
+	                            5: "May",
+	                            6: "June",
+	                            7: "July",
+	                            8: "August",
+	                            9: "September",
+	                            10: "October",
+	                            11: "November",
+	                            12 : "December"}, inplace=True)
+
+	try : 
+	    kg_saved_bf = kg_saved_bf.groupby(['month'])['kg_saved'].sum().reset_index()
+	    kg_saved_bf=kg_saved_bf.rename(columns={'kg_saved':'Breakfast'})
+
+	except :
+	    kg_saved_bf = pd.DataFrame(columns=['month','Breakfast'])
+	try:
+	    kg_saved_lunch = kg_saved_lunch.groupby(['month'])['kg_saved'].sum().reset_index()
+	    kg_saved_lunch=kg_saved_lunch.rename(columns={'kg_saved':'Lunch'})
+
+	except :
+	    kg_saved_lunch = pd.DataFrame(columns=['month','Lunch'])
+	try:
+	    kg_saved_din = kg_saved_din.groupby(['month'])['kg_saved'].sum().reset_index()
+	    kg_saved_din=kg_saved_din.rename(columns={'kg_saved':'Dinner'})
+
+	except:
+	    kg_saved_din = pd.DataFrame(columns=['month','Dinner'])
+
+	from functools import reduce
+
+	data_frame = [kg_saved_bf,kg_saved_lunch,kg_saved_din]
+	df_merged = reduce(lambda  left,right: pd.merge(left,right,on=['month'],
+	                                        how='outer'), data_frame)
+	df_merged = df_merged.fillna(0)
+	df_merged['total_saved'] = (df_merged['Breakfast']+df_merged['Lunch']+df_merged['Dinner']).round()
+	df_merged['Breakfast'] = df_merged['Breakfast'].round()
+	df_merged['Lunch'] = df_merged['Lunch'].round()
+	df_merged['Dinner'] = df_merged['Dinner'].round()
+	df_merged['total_saved'] = df_merged['total_saved'].round()
+
+
+	################################### DISPLAY SAVINGS
+
+	total_food_saved = round(df_merged['total_saved'].sum())
+	total_CO2_saved = round(df_merged['total_saved'].sum()*2.5)
+	total_USD_saved = round(df_merged['total_saved'].sum()*coef)
+
+	col_total_food_saved, col_total_CO2_saved, col_total_USD_saved = st.columns(3)
+	col_total_food_saved.metric("Total food saved", str(total_food_saved)+" Kg")
+	col_total_CO2_saved.metric("Total CO2 not emitted",str(total_CO2_saved)+" CO2 eq.")
+	col_total_USD_saved.metric("Total USD saved",str(total_USD_saved)+" USD")
+
+	################################### DISPLAY TABLE KG SAVED PER MONTH
+
+
+	fig_impact_table = go.Figure(data=[go.Table(
+	    header=dict(values=list(df_merged.columns),
+	                fill_color='pink',
+	                align='left'),
+	    cells=dict(values=[df_merged.month, df_merged.Breakfast, df_merged.Lunch, df_merged.Dinner,df_merged.total_saved],
+	               fill_color='lightgrey',
+	               align='left'))
+	])
+
+	fig_impact_table.update_layout(title_text="food saved by month and per shift (Kg)")
+
+	st.plotly_chart(fig_impact_table,use_container_width=True)
+
+	################################### DISPLAY BAR GRAPH PER DAY OF THE WEEK
+
+
+	waste_byday = df_new.copy()
+	waste_byday['date_waste'] = pd.to_datetime(waste_byday['date_waste'], format='%Y-%m-%d').dt.floor('d')
+
+
+	lundi = waste_byday.loc[waste_byday['date_waste'].dt.weekday == 0]
+	lundi['week_day'] = 'Monday'
+
+	mardi = waste_byday.loc[waste_byday['date_waste'].dt.weekday == 1]
+	mardi['week_day'] = 'Tuesday'
+
+
+	mercredi = waste_byday.loc[waste_byday['date_waste'].dt.weekday == 2]
+	mercredi['week_day'] = 'Wednesday'
+
+	jeudi = waste_byday.loc[waste_byday['date_waste'].dt.weekday == 3]
+	jeudi['week_day'] = 'Thursday'
+
+	vendredi = waste_byday.loc[waste_byday['date_waste'].dt.weekday == 4]
+	vendredi['week_day'] = 'Friday'
+
+	samedi = waste_byday.loc[waste_byday['date_waste'].dt.weekday == 5]
+	samedi['week_day'] = 'Saturday'
+
+	dimanche = waste_byday.loc[waste_byday['date_waste'].dt.weekday == 6]
+	dimanche['week_day'] = 'Sunday'
+
+
+
+
+
+	waste_byday = pd.concat([lundi,mardi,mercredi,jeudi,vendredi,samedi,dimanche])
+
+	waste_byday = waste_byday.groupby(['id_company','week_day','date_waste','covers'])['weight'].sum().reset_index()
+	waste_byday = waste_byday.groupby(['id_company','week_day'])['weight','covers'].sum().reset_index()
+	waste_byday['weight'] = waste_byday['weight']/waste_byday['covers']
+	waste_byday['weight'] = waste_byday['weight']*1000
+	waste_byday['weight'] = waste_byday['weight'].round()
+
+	waste_byday_fig = px.bar(waste_byday, x='week_day', y='weight', title='Waste/cover per day of the week - {}'.format(company),
+	                labels={'weight': 'weight/cover (g/cover)',
+	                        'week_day': 'Day',
+	                        'id_company': 'Company'},
+	                category_orders={"week_day": ["Monday", "Tuesday", "Wednesday", "Thursday","Friday",'Saturday','Sunday']},
+	                text = 'weight')
+
+	waste_byday_fig.update_traces(marker_color='thistle')
+
+	st.plotly_chart(waste_byday_fig,use_container_width=True)
+
+
+
 
 
 
